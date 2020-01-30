e1742464a0e9fbeca7b56b28192eef4ff3e00032
long76 <long76@users.noreply.github.com>
2020-01-30 21:54:43 +0300

    fix task jar and test

--- library/src/test/java/com/github/long76/androidxcolorpicker/ColorFormatHelperTest.java
+++ library/src/test/java/com/github/long76/androidxcolorpicker/ColorFormatHelperTest.java
@@ -1,18 +1,21 @@
 package com.github.long76.androidxcolorpicker;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import static com.github.long76.androidxcolorpicker.ColorFormatHelper.assertColorValueInRange;
 import static com.github.long76.androidxcolorpicker.ColorFormatHelper.formatColorValues;
+import static org.junit.Assert.assertEquals;
 
-public class ColorFormatHelperTest extends TestCase {
+public class ColorFormatHelperTest {
 
+    @Test
     public void testAssertColorValueInRange_allowedValues() throws Exception {
         for (int value = 0; value <= 255; value++) {
             assertEquals(value, assertColorValueInRange(value));
         }
     }
 
+    @Test
     public void testAssertColorValueInRange_disallowedValues() throws Exception {
         assertEquals(0, assertColorValueInRange(-1));
         assertEquals(0, assertColorValueInRange(-42));
@@ -25,6 +28,7 @@ public class ColorFormatHelperTest extends TestCase {
         assertEquals(0, assertColorValueInRange(1024));
     }
 
+    @Test
     public void testFormatColorValues_RgbValues() throws Exception {
         assertEquals("9ACD32", formatColorValues(154, 205, 50));
         assertEquals("000000", formatColorValues(0, 0, 0));
@@ -34,6 +38,7 @@ public class ColorFormatHelperTest extends TestCase {
         assertEquals("00FF00", formatColorValues(256, 255, 256));
     }
 
+    @Test
     public void testFormatColorValues_ArgbValues() throws Exception {
         assertEquals("429ACD32", formatColorValues(66, 154, 205, 50));
         assertEquals("00000000", formatColorValues(0, 0, 0, 0));
